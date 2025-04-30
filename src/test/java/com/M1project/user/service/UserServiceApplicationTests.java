4d4f713ff5e0ef1752f43dfe3742d11d2989bc16
heshanmadv <157682942+heshanmadv@users.noreply.github.com>
2025-04-30 22:07:39 +0530

    Initial commit

---
+++ src/test/java/com/M1project/user/service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.M1project.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
